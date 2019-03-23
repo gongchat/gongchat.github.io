c202d3e312548c57c0464e608e6378e1bcae5129
David Lee <devdavidlee@gmail.com>
2019-03-23 16:42:09 -0700

    update site

--- _posts/2019-03-19-release-notes-0.1.0.md
+++ _posts/2019-03-19-release-notes-0.1.0.md
@@ -3,4 +3,26 @@ layout: release-note
 title: Release 0.1.0
 ---
 
-Placeholder post, more details coming soon
+Welcome to the Alpha release of Gong! The goal is to build a customizable XMPP
+chat client with a modern UI. For more information please visit the [GitHub](https://github.com/gongchat/gong) page.
+
+If you find any issues or have any feature requests please leave a post at [github.com/gongchat/gong/issues](https://github.com/gongchat/gong/issues).
+
+### Release 0.1.1
+
+- Fix messages not updating when changing channels.
+- Fix exception when removing channels.
+
+### Release 0.1.2
+
+- Fix horizontal scroll bar for messages with long words.
+- Update message now says "Install After Restart" instead of "Later".
+- Can no longer open Gong twice, instead will focus on current Gong instance.
+- Added license
+- Added show option in tray for Windows
+- Routing of messages has been improved. When Gong encounters a user that is
+  logged into more than one chat client it will now send messages to the client
+  that it last received a message from. Before Gong relied on the XMPP server to
+  prioritize which client to send a message too.
+- Remove tray icon from Linux. The tray icon will only be used for status purposes moving forward. See the Electron [docs](https://electronjs.org/docs/api/tray#class-tray) for more details.
+- Included a .deb installation file for Linux users and is now the default file type for this site. Some users may have had issues adding Gong to the favorites bar with the previously format.
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,22 +1,101 @@
+const COLORS = [{
+    color1: '#333',
+    color2: '#444',
+    color3: '#555',
+    color4: '#666'
+  },
+  {
+    color1: '#182126',
+    color2: '#357CA8',
+    color3: '#AFC2D1',
+    color4: '#F66874'
+  },
+  {
+    color1: '#406B5A',
+    color2: '#44B2BF',
+    color3: '#C4F1F4',
+    color4: '#E5B270'
+  },
+  {
+    color1: '#FF8C9B',
+    color2: '#FFC8CB',
+    color3: '#FCEEBD',
+    color4: '#A1F15E'
+  },
+]
+
 $(document).ready(function() {
-  $('body').on('click', '.gong', function() {
-    const $this = $(this);
-    const $gongLogo = $this.parents('.gong-logo');
-    const clonedThis = $gongLogo.clone();
-    $gongLogo.replaceWith(clonedThis);
+  let updateColor = true;
+  let updateColorTimer;
+  let colorIndex = 0;
 
-    const $newGong = $('.gong-logo');
-    if (!$newGong.hasClass('gonged')) {
-      $newGong.addClass('gonged');
-    }
-  });
+  function attachGongOnClick() {
+    $('body').on('click', '.gong', function() {
+      const $this = $(this);
+      const $gongLogo = $this.parents('.gong-logo');
+      const clonedThis = $gongLogo.clone();
+      $gongLogo.replaceWith(clonedThis);
 
-  $('body').on('click', '.hamburger', function() {
-    const $menu = $('.menu');
-    if ($menu.hasClass('open')) {
-      $menu.removeClass('open');
-    } else {
-      $menu.addClass('open');
+      const $newGong = $('.gong-logo');
+      if (!$newGong.hasClass('gonged')) {
+        $newGong.addClass('gonged');
+      }
+    });
+  }
+
+  function attachHamburgerOnClick() {
+    $('body').on('click', '.hamburger', function() {
+      const $menu = $('.menu');
+      if ($menu.hasClass('open')) {
+        $menu.removeClass('open');
+      } else {
+        $menu.addClass('open');
+      }
+    });
+  }
+
+  function attachMiniGongMouseMove() {
+    const $sectionCustomize = $('.section-colors');
+    if ($sectionCustomize.length > 0) {
+      $('body').mousemove(function() {
+        if (updateColor) {
+          updateMiniGongColor();
+          updateColor = false;
+        }
+        if (!updateColorTimer) {
+          updateColorTimer = setTimeout(() => {
+            updateColor = true;
+            clearTimeout(updateColorTimer);
+            updateColorTimer = undefined;
+          }, 1000);
+        }
+      });
     }
-  });
+  }
+
+  function updateMiniGongColor() {
+    const $sectionCustomize = $('.section-colors');
+    if ($sectionCustomize.length > 0) {
+      const $color1 = $sectionCustomize.find('.color-1');
+      const $color2 = $sectionCustomize.find('.color-2');
+      const $color3 = $sectionCustomize.find('.color-3');
+      const $color4 = $sectionCustomize.find('.color-4');
+
+      // generate colors
+      if (colorIndex >= COLORS.length - 1) {
+        colorIndex = 0;
+      } else {
+        colorIndex++;
+      }
+
+      $color1.css('background-color', COLORS[colorIndex].color1);
+      $color2.css('background-color', COLORS[colorIndex].color2);
+      $color3.css('background-color', COLORS[colorIndex].color3);
+      $color4.css('background-color', COLORS[colorIndex].color4);
+    }
+  }
+
+  attachGongOnClick();
+  attachHamburgerOnClick();
+  attachMiniGongMouseMove();
 });
