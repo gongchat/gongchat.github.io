a3bc49e67242214b4f6d1502b9cb60f1c082c17c
David Lee <devdavidlee@gmail.com>
2019-03-24 00:51:55 -0700

    update site

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -22,12 +22,30 @@ const COLORS = [{
     color3: '#FCEEBD',
     color4: '#A1F15E'
   },
-]
+];
+
+const FONTS = [
+  `'Merriweather', serif`,
+  `'Raleway', sans-serif`,
+  `'Roboto Slab', serif`,
+  `'Amatic SC', cursive`,
+  `'Dancing Script', cursive`,
+  `'Indie Flower', cursive`,
+  `'Libre Barcode 39', cursive`,
+  `'Open Sans Condensed', sans-serif`,
+];
 
 $(document).ready(function() {
   let updateColor = true;
   let updateColorTimer;
   let colorIndex = 0;
+
+  let updateFontFamily = true;
+  let updateFontFamilyTimer;
+  let fontIndex = 0;
+
+  let updateEmojis = true;
+  let updateEmojisTimer;
 
   function attachGongOnClick() {
     $('body').on('click', '.gong', function() {
@@ -35,7 +53,6 @@ $(document).ready(function() {
       const $gongLogo = $this.parents('.gong-logo');
       const clonedThis = $gongLogo.clone();
       $gongLogo.replaceWith(clonedThis);
-
       const $newGong = $('.gong-logo');
       if (!$newGong.hasClass('gonged')) {
         $newGong.addClass('gonged');
@@ -54,48 +71,163 @@ $(document).ready(function() {
     });
   }
 
-  function attachMiniGongMouseMove() {
-    const $sectionCustomize = $('.section-colors');
-    if ($sectionCustomize.length > 0) {
-      $('body').mousemove(function() {
-        if (updateColor) {
-          updateMiniGongColor();
-          updateColor = false;
-        }
-        if (!updateColorTimer) {
-          updateColorTimer = setTimeout(() => {
-            updateColor = true;
-            clearTimeout(updateColorTimer);
-            updateColorTimer = undefined;
-          }, 1000);
-        }
+  //
+  // Mini Gong
+  //
+
+  function attachMiniGongOnMouseMove() {
+    $('.section-colors').mousemove(function() {
+      updateMiniGongColor();
+    });
+  }
+
+  function attachMiniGongOnScroll() {
+    const $section = $('.section-colors');
+    if ($section.length > 0) {
+      $(window).scroll(function() {
+        updateMiniGongColor();
       });
     }
   }
 
   function updateMiniGongColor() {
-    const $sectionCustomize = $('.section-colors');
-    if ($sectionCustomize.length > 0) {
-      const $color1 = $sectionCustomize.find('.color-1');
-      const $color2 = $sectionCustomize.find('.color-2');
-      const $color3 = $sectionCustomize.find('.color-3');
-      const $color4 = $sectionCustomize.find('.color-4');
-
-      // generate colors
-      if (colorIndex >= COLORS.length - 1) {
-        colorIndex = 0;
-      } else {
-        colorIndex++;
+    if (updateColor) {
+      const $section = $('.section-colors');
+      if ($section.length > 0) {
+        const $color1 = $section.find('.color-1');
+        const $color2 = $section.find('.color-2');
+        const $color3 = $section.find('.color-3');
+        const $color4 = $section.find('.color-4');
+        // generate colors
+        if (colorIndex >= COLORS.length - 1) {
+          colorIndex = 0;
+        } else {
+          colorIndex++;
+        }
+        $color1.css('background-color', COLORS[colorIndex].color1);
+        $color2.css('background-color', COLORS[colorIndex].color2);
+        $color3.css('background-color', COLORS[colorIndex].color3);
+        $color4.css('background-color', COLORS[colorIndex].color4);
       }
-
-      $color1.css('background-color', COLORS[colorIndex].color1);
-      $color2.css('background-color', COLORS[colorIndex].color2);
-      $color3.css('background-color', COLORS[colorIndex].color3);
-      $color4.css('background-color', COLORS[colorIndex].color4);
+      updateColor = false;
+    }
+    if (!updateColorTimer) {
+      updateColorTimer = setTimeout(() => {
+        updateColor = true;
+        clearTimeout(updateColorTimer);
+        updateColorTimer = undefined;
+      }, 1000);
     }
   }
 
+  //
+  // Font
+  //
+
+  function attachFontOnScroll() {
+    $('.section-fonts').mousemove(function() {
+      updateTextFontFamily();
+    });
+  }
+
+  function attachFontOnMouseMove() {
+    const $section = $('.section-fonts');
+    if ($section.length > 0) {
+      $section.mousemove(function() {
+        updateTextFontFamily();
+      });
+    }
+  }
+
+  function updateTextFontFamily() {
+    if (updateFontFamily) {
+      const $section = $('.section-fonts');
+      if ($section.length > 0) {
+        const $text = $section.find('.dynamic-google-font');
+        // generate colors
+        if (fontIndex >= FONTS.length - 1) {
+          fontIndex = 0;
+        } else {
+          fontIndex++;
+        }
+        $text.css('font-family', FONTS[fontIndex]);
+      }
+      updateFontFamily = false;
+    }
+    if (!updateFontFamilyTimer) {
+      updateFontFamilyTimer = setTimeout(() => {
+        updateFontFamily = true;
+        clearTimeout(updateFontFamilyTimer);
+        updateFontFamilyTimer = undefined;
+      }, 1000);
+    }
+  }
+
+  //
+  // Emojis 
+  //
+
+  function attachEmojisOnMouseMove() {
+    const $section = $('.section-markdown');
+    if ($section.length > 0) {
+      $section.mousemove(function() {
+        randomizeEmojis();
+      });
+    }
+  }
+
+  function randomizeEmojis() {
+    if (updateEmojis) {
+      const $section = $('.section-markdown');
+      if ($section.length > 0) {
+        const $emojis = $section.find('.emojis');
+        $emojis.html(getShuffledArr($emojis.children()));
+      }
+      updateEmojis = false;
+    }
+    if (!updateEmojisTimer) {
+      updateEmojisTimer = setTimeout(() => {
+        updateEmojis = true;
+        clearTimeout(updateEmojisTimer);
+        updateEmojisTimer = undefined;
+      }, 1000);
+    }
+  }
+
+  //
+  // Utilities
+  //
+
+  function getShuffledArr(arr) {
+    const newArr = arr.slice()
+    for (let i = newArr.length - 1; i > 0; i--) {
+      const rand = Math.floor(Math.random() * (i + 1));
+      [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
+    }
+    return newArr
+  };
+
+  function emojiStringToArray(str) {
+    split = str.split(/([\uD800-\uDBFF][\uDC00-\uDFFF])/);
+    arr = [];
+    for (var i = 0; i < split.length; i++) {
+      char = split[i]
+      if (char !== "") {
+        arr.push(char);
+      }
+    }
+    return arr;
+  };
+
+  // attach all the things
   attachGongOnClick();
   attachHamburgerOnClick();
-  attachMiniGongMouseMove();
+
+  // attachMiniGongOnScroll();
+  attachMiniGongOnMouseMove();
+
+  // attachFontOnScroll();
+  attachFontOnMouseMove();
+
+  attachEmojisOnMouseMove();
 });
